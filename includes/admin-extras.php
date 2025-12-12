78df50166ef00a76fc6ee66a9bac46057b24f08b
Paco Toledo <pacotole@crea.me>
2025-12-12 11:34:30 +0100

    move affiliate links to plugin links

--- includes/admin-extras.php
+++ includes/admin-extras.php
@@ -37,15 +37,6 @@ class AdminExtras {
 		$link_polylang  = '';
 		$link_elementor = '';
 
-		// Custom Affiliate Link
-		$link_affiliate = sprintf(
-			'<a href="%1$s" title="%2$s" target="_blank" rel="noopener noreferrer">%3$s</a>',
-			esc_url( 'https://coolplugins.net/product/autopoly-ai-translation-for-polylang/?ref=creame&utm_source=cpel_plugin&utm_medium=inside&utm_campaign=get_pro&utm_content=plugins_list' ),
-			esc_html__( 'AI Translation For Polylang', 'connect-polylang-elementor' ),
-			esc_html__( 'AI Translation (Affiliated)', 'connect-polylang-elementor' )
-		);
-
-
 		/** Add settings link only if user has permission */
 		if ( current_user_can( 'edit_theme_options' ) ) {
 
@@ -81,17 +72,8 @@ class AdminExtras {
 			array_unshift( $cpel_links, $link_polylang, $link_elementor );
 		}
 
-		// Prepend this link
-    	array_unshift( $cpel_links, $link_affiliate );
-
-		/** Display plugin settings links */
-		
-		// Allow filters to modify
-		return apply_filters(
-			'cpel/filter/plugins_page/settings_links',
-			$cpel_links,
-			$link_affiliate
-		);
+		// Output the links.
+		return apply_filters( 'cpel/filter/plugins_page/settings_links', $cpel_links );
 
 	}
 
@@ -108,8 +90,13 @@ class AdminExtras {
 
 		if ( CPEL_BASENAME === $cpel_file ) {
 
-			$cpel_links[] = '<a href="https://paypal.me/pacotole" title="Support this plugin\'s development">Donate</a>';
-			$cpel_links[] = '<a href="mailto:wespeakcomputer@gmail.com" title="Request personal one on one training on using Polylang + Elementor">Personal one-on-one training</a>';
+			$cpel_links[] = sprintf(
+				'<a href="%1$s" title="%2$s" target="_blank" rel="noopener noreferrer">%3$s <small><b>(%4$s)</b></small></a>',
+				esc_url( 'https://coolplugins.net/product/autopoly-ai-translation-for-polylang/?ref=creame&utm_source=cpel_plugin&utm_medium=inside&utm_campaign=get_pro&utm_content=plugins_list' ),
+				esc_html__( 'AI Translation For Polylang', 'connect-polylang-elementor' ),
+				esc_html__( 'AI Translation', 'connect-polylang-elementor' ),
+				esc_html__( 'Affiliated', 'connect-polylang-elementor' ),
+			);
 
 		}
 
